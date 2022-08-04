b3b1c6024db5f97765ebbf03b1bd4984132e10d5
HieuFoden <trunghieu.foden@gmail.com>
2022-08-04 14:47:21 +0700

    video11

---
+++ src/configs/connectDB.js
@@ -0,0 +1,18 @@
+// get the client
+import mysql from 'mysql2/promise';
+
+// create the connection to database
+console.log('Creating connection pool...');
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'nodejsbasic'
+})
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'nodejsbasic'
+// });
+
+export default pool;
--- src/route/web.js
+++ src/route/web.js
@@ -3,7 +3,9 @@ import homeController from '../controller/homeController'
 
 let router = express.Router();
 const initWebRoute = (app) => {
+
     router.get('/', homeController.getHomepage);
+    router.get('/detail/user/:id', homeController.getDetailPage);
     router.get('/about', (req, res) => {
         res.send('Pham Trung Hieu')
     });
